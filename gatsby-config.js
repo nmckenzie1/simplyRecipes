e38e8d812c2d7b00f5727569c94b1283112300ba
Neil M <n.mckenzie@live.com>
2021-08-16 18:51:18 -0500

    added .env

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,9 @@
  *
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
-
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
 module.exports = {
   siteMetadata: {
     title: "Simply Recipes",
@@ -22,7 +24,7 @@ module.exports = {
       options: {
         spaceId: `c4aej6av4w32`,
         // Learn about environment variables: https://gatsby.dev/env-vars
-        accessToken: `BbiPT-_TtUrw9YGSsBPIEPnNEH9CDrL5WKjGiWNVti4`,
+        accessToken: process.env.CONTENTFUL_API_KEY,
       },
     },
 
